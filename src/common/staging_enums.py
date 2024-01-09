f45c165329e29668c56a7e6fe227faeb315a07cc
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-01-09 09:17:49 -0500

    adding enums for the staging types

---
+++ src/common/staging_enums.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
+# SPDX-FileCopyrightText: 2023 Renaissance Computing Institute. All rights reserved.
+# SPDX-FileCopyrightText: 2024 Renaissance Computing Institute. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
+# SPDX-License-Identifier: MIT
+
+"""
+    Class enums for the project
+
+    Author: Phil Owen, RENCI.org
+"""
+
+from enum import Enum
+
+
+class StagingType(str, Enum):
+    """
+    Class to represent the staging states
+
+    """
+    # initial staging
+    INITIAL_STAGING = 'initial'
+
+    # final staging
+    FINAL_STAGING = 'final'
