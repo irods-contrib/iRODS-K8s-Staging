eb77e28e7553d48dd1bff902f8c7289d5231ec01
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-04-01 15:41:07 -0400

    creating the destination dir for test results on the NFS store

--- src/staging/staging.py
+++ src/staging/staging.py
@@ -216,8 +216,14 @@ class Staging:
                         if run_data['request_data']['package-dir']:
                             self.default_pkg_dir = run_data['request_data']['package-dir']
 
+                        # get the directory path
+                        archive_dir: str = str(os.path.join(self.default_pkg_dir, run_data['request_group'], str(run_data['id'])))
+
+                        # create the command to create the directory
+                        fp.write(f'echo "Creating archive directory {archive_dir}..."; mkdir -p {archive_dir};\n')
+
                         # get the full path to the test results archive file
-                        archive_file = os.path.join(self.default_pkg_dir, run_data['request_group'], str(run_data['id']), f'{executor}.tar.gz')
+                        archive_file: str = str(os.path.join(archive_dir, f'{executor}.tar.gz'))
 
                         # compress the directory into the package directory
                         fp.write(f'echo "Zip and Tar the results dir {data_path} into {archive_file}"; tar -zcvf "{archive_file}" {data_path};\n')
